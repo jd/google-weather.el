d59f6f6d12b3373f399050e2aa8629cc6f6a196d
Jason F. McBrayer <McBrayerJ@dnr.sc.gov>
2010-09-09 11:00:49 -0400

    Missing . in org-google-weather-icon-alist
    Signed-off-by: Julien Danjou <julien@danjou.info>

--- org-google-weather.el
+++ org-google-weather.el
@@ -62,7 +62,7 @@
 (defcustom org-google-weather-icon-alist
   '((chance_of_rain . "weather-showers-scattered.png")
     (chance_of_snow . "weather-snow.png")
-    (chance_of_storm "weather-storm.png")
+    (chance_of_storm . "weather-storm.png")
     (cloudy . "weather-overcast.png")
     (dust . "weather-fog.png")
     (flurries . "weather-storm.png")
